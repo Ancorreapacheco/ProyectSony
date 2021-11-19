d2a079a70c4ff00866329ce278a1f140c3e4f845
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-11-19 18:31:03 -0500

    Commit

---
+++ server/src/config/database.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://Equipo4:ProyectoSony1234@cluster0.bhd6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,21 @@
-const express = require('express');
+const Express = require('express');
+const Path = require('path');
+const Mongoose = require('mongoose');
+const Passport = require('passport');
+const Flash = require('connect-flash');
+const Morgan = require('morgan');
+const CookieParser = require('cookie-parser');
+const BodyParser = require('body-parser');
+const Session = require('express-session');
 
-const app = express();
+const { url } = require('./config/databse');
+const { truncate } = require('fs');
+
+Mongoose.connect(url, {
+    useMongoClient: truncate
+});
+
+const app = Express();
 
 app.set('port', process.env.PORT || 5000);
 
