4963b8a60b5a817e1bad2d3afea7f2c569bdeaa8
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-11-19 20:41:35 -0500

    Update database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://Equipo4:ProyectoSony1234@cluster0.bhd6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
