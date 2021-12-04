7b280644932dc3175f6290951a929c7379bda38f
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-12-04 17:29:58 -0500

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Home } from './layout/home';
+import { Cuenta } from './layout/cuenta';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
     <Router>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/cuenta' element={< Cuenta />}/>
       </Routes>
     </Router>
   );
