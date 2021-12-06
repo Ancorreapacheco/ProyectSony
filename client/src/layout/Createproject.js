3041bdc02e0366e2727c652c9584afc5d760bcb4
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-12-06 10:55:13 -0500

    Commit

--- client/src/layout/Createproject.js
+++ client/src/layout/Createproject.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './css/style_main.css';
+import './css/nucleo-icons.css';
+import './css/nucleo-svg.css';
 import Logo from './images/logosony.png';
 
 export const Createproject = () => {
@@ -196,6 +198,96 @@ export const Createproject = () => {
                         </div>
                     </div>
                 </nav>
+                <div>
+                    <div style={{ float: 'left', width: '70%', height: '100px', padding: '10px' }}>
+                        <hr></hr>
+                        <h2>
+                            Crear proyecto
+                        </h2>
+                        <hr></hr>
+                    </div>
+                    <div style={{ float: 'left', width: '30%', height: '100px', padding: '10px' }}>
+                        <br></br>
+                        <form runat="server">
+                            <input accept="image/*" type='file' id="imgInp" />
+                            <img id="blah" src="#" alt="your image" />
+                            <p>Selecciona la imagen del proyecto</p>
+                        </form>
+                    </div>
+                </div>
+                <br></br>
+                <div>
+                    <br></br>
+                    <div style={{  marginTop: '80px' }}>
+                        <form>
+                        <div>
+                            <label for="start">
+                                Fecha de inicio: &emsp;
+                            </label>
+                            <input type="date" id="start"></input>
+                        </div>
+                        <div>
+                            <label for="finish">
+                                Fecha de cierre: &emsp;
+                            </label>
+                            <input type="date" id="finish"></input>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+                <hr></hr>
+                <div>
+                    <div>
+                        <label for="title">
+                            Título: &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                        </label>
+                        <input type="text" id="title"></input>
+                    </div>
+                    <br></br>
+                    <div>
+                        <label for="description">
+                            Descripción: &emsp;&emsp;&emsp;&emsp;&emsp;
+                        </label>
+                        <textarea id="description" style = {{ width: '500px' , borderRadius: '10px'}}>Escribe aquí....</textarea>
+                    </div>
+                </div>
+                <br></br>
+                <h4>Asignar usuarios subalternos</h4>
+                <br></br>
+                <div style={{ color: 'black' }}>
+                    <div style={{ float: 'left', width: '60%', height: '100px', padding: '10px' }}>
+                        <div style={{ display: 'flex' }}>
+                            <label for="ID">
+                                Filtrar por ID: &emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
+                            </label>
+                            <input type="text" id="ID"></input>
+                            <div style={{ padding: '0 25px' }}>
+                                <button style={{ borderRadius: '10px' }}><i className="fas fa-search"></i></button>
+                            </div>
+                            <div style={{ padding: '0 25px' }}>
+                                <button style={{ borderRadius: '10px' }}>Agregar</button>
+                            </div>
+                        </div>
+                        <br></br>
+                        <div style={{ display: 'flex' , justifyContent: 'initial'}}>
+                            <label for="name">
+                                Filtrar por nombre: &emsp;&emsp;
+                            </label>
+                            <input type="text" id="name"></input>
+                            <div style={{ padding: '0 25px' }}>
+                                <button style={{ borderRadius: '10px' }}><i className="fas fa-search"></i></button>
+                            </div>
+                            <div style={{ padding: '0 25px'}}>
+                                <button style={{ borderRadius: '10px' }}>Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ float: 'left', width: '40%', height: '100px', padding: '10px' }}>
+                        <h5>Usuarios subalternos asociados al proyecto</h5>
+                        <br></br>
+                        <textarea style={{ borderRadius: '10px', width: '500px', height: '200px'}}></textarea>
+                    </div>
+                </div>
             </main>    
         </div>
     )
