998fa69a4923a75faca957ec1b78e18aad87833e
FlorezLina <91763261+FlorezLina@users.noreply.github.com>
2021-12-09 14:28:36 -0500

    Anexo Report.js y cambios en app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Cuenta } from './layout/Count';
 import { Login } from './layout/Login';
 import { Register } from './layout/Register';
 import { Projects } from './layout/Projects';
+import { Report } from './layout/Report';
 import { Task } from './layout/Task';
 import { Createproject } from './layout/Createproject';
 import { Notifications } from './layout/Notifications';
@@ -23,6 +24,7 @@ function App(){
           <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/projects/create/task' element={<Task />} />
           <Route exact path='/projects/create' element={<Createproject />} />
+          <Route exact path='/report' element={<Report />} />
           <Route exact path='/notifications' element={<Notifications />} />
         </Routes>
       </Fragment>
