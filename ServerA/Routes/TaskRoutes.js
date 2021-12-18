6ee18b10d99e73265d58e52a5040aede9bde6eb6
Andrés Correa <aacorrea@uninorte.edu.co>
2021-12-18 17:52:21 -0500

    Trabajando en login

--- ServerA/Routes/TaskRoutes.js
+++ ServerA/Routes/TaskRoutes.js
@@ -2,12 +2,15 @@
 const express = require("express");
 const router = express.Router();
 
-//Llamó al modelo de datos
+//Llamo al modelo de datos
 
 const Task = require("../models/task");
+const userSchema= require('../models/userSchema');
 
+
+//----------RUTAS PARA LOS PROYTECTOS (MAL LLAMADOS TAREAS)---------------------
 //Obtener todas las tareas
-router.get("/api/task", async (req, res) => {
+router.get("/projects", async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
     console.log('Info cargada.');
@@ -16,16 +19,16 @@ router.get("/api/task", async (req, res) => {
 
 //Obtener tareas por ID
 
-router.get('/api/task/:id', async(req, res) => {
+router.get('/projects/:id', async(req, res) => {
     const task= await Task.findById(req.params.id);
     res.json(task);    
     res.end();
 })
 
 //Crear tareas
-router.post('/api/task', async (req, res) => {
-    const {title, description} = req.body;
-    const task= new Task ({title, description});    
+router.post('/projects', async (req, res) => {
+    const {title, description, start_date, finish_date} = req.body;
+    const task= new Task ({title, description, start_date, finish_date});    
     await task.save();
     res.json({status:'Tarea almacenada'});
     console.log('Tarea almacenada - msj consola')
@@ -34,9 +37,9 @@ router.post('/api/task', async (req, res) => {
 
 //Actualizar Tareas por ID
 
-router.put('/api/task/:id', async(req,res) =>{
-    const {title,description}= req.body;
-    const updateTask= {title,description};
+router.put('/projects/:id', async(req,res) =>{
+    const {title,description,start_date,finish_date}= req.body;
+    const updateTask= {title,description,start_date, finish_date};
     await Task.findByIdAndUpdate(req.params.id, updateTask);
     res.json({status:'Tarea Modificada'})
     res.end();
@@ -45,10 +48,22 @@ router.put('/api/task/:id', async(req,res) =>{
 
 //Eliminar tareas por ID
 
-router.delete('/api/task/:id', async(req, res) => {
+router.delete('/projects/:id', async(req, res) => {
     await Task.findByIdAndDelete(req.params.id);
     res.json({status:"registro eliminado."});
     res.end();
 })
+
+
+//-------------------USUARIOS------------------------//
+
+//Obtener tareas por ID
+
+router.get('/login/:email', async(req, res) => {
+    const userschema = await userSchema.findOne(req.params.email);    
+    res.json(userschema);    
+    res.end();
+})
+
 
 module.exports = router;
